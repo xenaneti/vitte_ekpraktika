81b3eda41f0ee6e8a5db06911d6e7b224670baea
xenaneti <alex846brown7672@gmail.com>
2026-09-24 08:30:12 +0300

    сделал страницы и меню сайта

---
+++ baths/context_processors.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+
+def site(request):
+    return {
+        "site_name": "Тихий пар",
+        "site_url": settings.SITE_URL,
+        "canonical_url": settings.SITE_URL + request.path,
+        "navigation": [
+            ("baths", "Бани и сауны"), ("services", "Услуги и цены"),
+            ("about", "О комплексе"), ("journal", "Журнал"),
+            ("news", "Новости"), ("contacts", "Контакты"),
+        ],
+    }
